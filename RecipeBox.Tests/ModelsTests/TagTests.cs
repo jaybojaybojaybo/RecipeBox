5b9157cf4c1b501884c6894aa3939077b40a8952
Jasun Feddema and Nico Daunt <nico@>
2018-02-28 11:29:21 -0800

    create green test condition for Tag CRUD

--- RecipeBox.Tests/ModelsTests/TagTests.cs
+++ RecipeBox.Tests/ModelsTests/TagTests.cs
@@ -17,7 +17,74 @@ namespace RecipeBox.Tests
       {
         Tag.DeleteAll();
         Recipe.DeleteAll();
-        Ingredient.DeleteAll();
+      }
+
+      [TestMethod]
+      public void Equals_ReturnsTrueIfNamesAreTheSame_Tag()
+      {
+        // Arrange
+        Tag firstTag = new Tag("Artichoke");
+        Tag secondTag = new Tag("Artichoke");
+
+        //Act
+        firstTag.Save();
+        secondTag.Save();
+
+
+        // Assert
+        Assert.AreEqual(true, firstTag.GetName().Equals(secondTag.GetName()));
+      }
+
+      [TestMethod]
+      public void Find_FindsTagInDatabase_Tag()
+      {
+        //Arrange
+        Tag testTag = new Tag("Artichoke");
+        testTag.Save();
+
+        //Act
+        Tag foundTag = Tag.Find(testTag.GetId());
+
+        //Assert
+        Assert.AreEqual(testTag, foundTag);
+      }
+
+      [TestMethod]
+      public void Edit_UpdateTagInDatabase_String()
+      {
+        //Arrange
+        string firstName = "Artichoke";
+        Tag testTag = new Tag(firstName);
+        testTag.Save();
+        string secondName = "Red Pepper";
+
+        //Act
+        testTag.Edit(secondName);
+        string result = Tag.Find(testTag.GetId()).GetName();
+
+        //Assert
+        Assert.AreEqual(secondName, result);
+      }
+
+      [TestMethod]
+      public void Delete_DeleteOneTagInDatabase_True()
+      {
+        //Arrange
+        string firstName = "Artichokes";
+        string secondName = "Red Peppers";
+        Tag firstTag = new Tag(firstName);
+        Tag secondTag = new Tag(secondName);
+        List<Tag> testList = new List<Tag>{secondTag};
+        firstTag.Save();
+        secondTag.Save();
+
+        //Act
+        int firstId = firstTag.GetId();
+        firstTag.DeleteTag(firstId);
+        List<Tag> compareList = Tag.GetAll();
+        Console.WriteLine(compareList.Count);
+        //Assert
+        Assert.AreEqual(testList.Count, compareList.Count);
       }
   }
 }
--- RecipeBox/Models/Tag.cs
+++ RecipeBox/Models/Tag.cs
@@ -9,12 +9,14 @@ namespace RecipeBox.Models
   public class Tag
   {
     private int _id;
-    private string _tagName;
+    private string _name;
+
+
 
     public Tag(string tagName, int Id = 0)
     {
       _id = Id;
-      _tagName = tagName;
+      _name = tagName;
     }
 
     public static void DeleteAll()
@@ -34,25 +36,171 @@ namespace RecipeBox.Models
       }
     }
 
-    // public override bool Equals(System.Object otherTag)
-    // {
-    //   if (!(otherTag is Tag))
-    //   {
-    //     return false;
-    //   }
-    //   else
-    //   {
-    //     Tag newTag = (Tag) otherTag;
-    //     bool idEquality = (this.GetId() == newTag.GetId());
-    //     bool nameEquality = (this.GetName() == newTag.GetName());
-    //     return (idEquality && nameEquality);
-    //   }
-    // }
-    //
-    // public override int GetHashCode()
-    // {
-    //   return this.GetId().GetHashCode();
-    // }
+    public override bool Equals(System.Object otherTag)
+    {
+      if (!(otherTag is Tag))
+      {
+        return false;
+      }
+      else
+      {
+        Tag newTag = (Tag) otherTag;
+        bool idEquality = (this.GetId() == newTag.GetId());
+        bool nameEquality = (this.GetName() == newTag.GetName());
+        return (idEquality && nameEquality);
+      }
+    }
+
+    public override int GetHashCode()
+    {
+      return this.GetId().GetHashCode();
+    }
+
+    public int GetId()
+    {
+      return _id;
+    }
+
+    public void SetId(int newId)
+    {
+      _id = newId;
+    }
+
+    public string GetName()
+    {
+      return _name;
+    }
+
+    public void SetName(string newTagName)
+    {
+      _name = newTagName;
+    }
+
+    public void Save()
+    {
+      MySqlConnection conn = DB.Connection();
+     conn.Open();
+
+     var cmd = conn.CreateCommand() as MySqlCommand;
+    cmd.CommandText = @"INSERT INTO tags (name) VALUES (@TagName);";
+     cmd.Parameters.Add(new MySqlParameter("@TagName", this._name));
+
+     cmd.ExecuteNonQuery();
+     _id = (int) cmd.LastInsertedId;
+
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+    }
+
+    public static Tag Find(int id)
+    {
+     MySqlConnection conn = DB.Connection();
+     conn.Open();
+
+     var cmd = conn.CreateCommand() as MySqlCommand;
+     cmd.CommandText = @"SELECT * FROM tags WHERE id = @searchId;";
+
+     MySqlParameter searchId = new MySqlParameter();
+     searchId.ParameterName = "@searchId";
+     searchId.Value = id;
+     cmd.Parameters.Add(searchId);
+
+     var rdr = cmd.ExecuteReader() as MySqlDataReader;
+
+     int tagId = 0;
+     string tagName = "";
+
+     while (rdr.Read())
+     {
+       tagId = rdr.GetInt32(0);
+       tagName = rdr.GetString(1);
+     }
+
+     Tag foundTag= new Tag(tagName, tagId);
+
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+
+     return foundTag;
+    }
+
+    public void Edit(string newName)
+    {
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"UPDATE tags SET name = @newName WHERE id = @searchId;";
+
+      MySqlParameter searchId = new MySqlParameter();
+      searchId.ParameterName = "@searchId";
+      searchId.Value = _id;
+      cmd.Parameters.Add(searchId);
+
+      MySqlParameter name = new MySqlParameter();
+      name.ParameterName = "@newname";
+      name.Value = newName;
+      cmd.Parameters.Add(name);
+
+      cmd.ExecuteNonQuery();
+      _name = newName;
+
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+    }
+
+    public static List<Tag> GetAll()
+    {
+      List<Tag> allTags = new List<Tag> {};
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"SELECT * FROM tags;";
+      var rdr = cmd.ExecuteReader() as MySqlDataReader;
+      while(rdr.Read())
+      {
+        int tagId = rdr.GetInt32(0);
+        string tagName = rdr.GetString(1);
+        Tag newTag = new Tag(tagName, tagId);
+        allTags.Add(newTag);
+      }
+      conn.Close();
+      if (conn != null)
+      {
+          conn.Dispose();
+      }
+      return allTags;
+    }
+
+    public void DeleteTag(int id)
+    {
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"DELETE FROM tags WHERE id = @searchId;";
+
+      MySqlParameter searchId = new MySqlParameter();
+      searchId.ParameterName = "@searchId";
+      searchId.Value = _id;
+      cmd.Parameters.Add(searchId);
+
+      cmd.ExecuteNonQuery();
+
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+    }
 
 
   }
