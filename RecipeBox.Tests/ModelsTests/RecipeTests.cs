bdae019a84ccdec2367a6f3361579348cfd9510b
Jasun Feddema and Nico Daunt <nico@>
2018-02-28 11:23:37 -0800

    create green test condition for Recipe Delete test methods

--- RecipeBox.Tests/ModelsTests/RecipeTests.cs
+++ RecipeBox.Tests/ModelsTests/RecipeTests.cs
@@ -19,5 +19,106 @@ namespace RecipeBox.Tests
         Ingredient.DeleteAll();
         Tag.DeleteAll();
       }
+
+      [TestMethod]
+      public void Equals_ReturnsTrueIfNamesAreTheSame_Recipe()
+      {
+        // Arrange
+        Recipe firstRecipe = new Recipe("Roasted Artichokes", "5", "cook");
+        Recipe secondRecipe = new Recipe("Roasted Artichokes", "5", "cook");
+
+        //Act
+        firstRecipe.Save();
+        secondRecipe.Save();
+
+        // Assert
+        Assert.AreEqual(true, firstRecipe.GetName().Equals(secondRecipe.GetName()));
+      }
+
+      [TestMethod]
+      public void Find_FindsRecipeInDatabase_Recipe()
+      {
+        //Arrange
+        Recipe testRecipe = new Recipe("Roasted Artichokes", "5", "cook");
+        testRecipe.Save();
+
+        //Act
+        Recipe foundRecipe = Recipe.Find(testRecipe.GetId());
+
+        //Assert
+        Assert.AreEqual(testRecipe.GetId(), foundRecipe.GetId());
+      }
+
+      [TestMethod]
+      public void EditName_UpdateRecipeInDatabase_String()
+      {
+        //Arrange
+        string firstName = "Artichoke";
+        Recipe testRecipe = new Recipe(firstName, "5", "cook");
+        testRecipe.Save();
+        string secondName = "Red Pepper";
+
+        //Act
+        testRecipe.EditName(secondName);
+        string result = Recipe.Find(testRecipe.GetId()).GetName();
+
+        //Assert
+        Assert.AreEqual(secondName, result);
+      }
+
+      [TestMethod]
+      public void EditRating_UpdateRecipeInDatabase_String()
+      {
+        //Arrange
+        string firstRating = "5";
+        Recipe testRecipe = new Recipe("Roasted Artichokes", firstRating, "cook");
+        testRecipe.Save();
+        string secondRating = "4";
+
+        //Act
+        testRecipe.EditRating(secondRating);
+        string result = Recipe.Find(testRecipe.GetId()).GetRating();
+
+        //Assert
+        Assert.AreEqual(secondRating, result);
+      }
+
+      [TestMethod]
+      public void EditInstruction_UpdateRecipeInDatabase_String()
+      {
+        //Arrange
+        string firstInstruction = "cook";
+        Recipe testRecipe = new Recipe("Roasted Artichokes", "5", firstInstruction);
+        testRecipe.Save();
+        string secondInstruction = "don't cook";
+
+        //Act
+        testRecipe.EditInstruction(secondInstruction);
+        string result = Recipe.Find(testRecipe.GetId()).GetInstruction();
+
+        //Assert
+        Assert.AreEqual(secondInstruction, result);
+      }
+
+      [TestMethod]
+      public void Delete_DeleteOneRecipeInDatabase_True()
+      {
+        //Arrange
+        Recipe firstRecipe = new Recipe("Roasted Artichokes", "5", "cook");
+        Recipe secondRecipe = new Recipe("Spaghetti", "3", "boil");
+        List<Recipe> testList = new List<Recipe>{secondRecipe};
+        firstRecipe.Save();
+        secondRecipe.Save();
+
+        //Act
+        int firstId = firstRecipe.GetId();
+        firstRecipe.DeleteRecipe(firstId);
+        List<Recipe> compareList = Recipe.GetAll();
+        Console.WriteLine(compareList.Count);
+        //Assert
+        Assert.AreEqual(testList.Count, compareList.Count);
+      }
   }
+
+
 }
--- RecipeBox/Models/Recipe.cs
+++ RecipeBox/Models/Recipe.cs
@@ -9,14 +9,14 @@ namespace RecipeBox.Models
   public class Recipe
   {
     private int _id;
-    private string _recipeName;
+    private string _name;
     private string _rating;
     private string _instruction;
 
     public Recipe(string recipeName, string rating, string instruction, int Id = 0)
     {
       _id = Id;
-      _recipeName = recipeName;
+      _name = recipeName;
       _rating = rating;
       _instruction = instruction;
     }
@@ -38,26 +38,235 @@ namespace RecipeBox.Models
       }
     }
 
-    // public override bool Equals(System.Object otherRecipe)
-    // {
-    //   if (!(otherRecipe is Recipe))
-    //   {
-    //     return false;
-    //   }
-    //   else
-    //   {
-    //     Recipe newRecipe = (Recipe) otherRecipe;
-    //     bool idEquality = (this.GetId() == newRecipe.GetId());
-    //     bool nameEquality = (this.GetName() == newRecipe.GetName());
-    //     bool ratingEquality = (this.GetRating() == newRecipe.GetRating());
-    //     bool instructionEquality = (this.GetInstruction() == newRecipe.GetInstruction());
-    //     return (idEquality && nameEquality && ratingEquality && instructionEquality);
-    //   }
-    // }
-    //
-    // public override int GetHashCode()
-    // {
-    //   return this.GetId().GetHashCode();
-    // }
+    public override bool Equals(System.Object otherRecipe)
+    {
+      if (!(otherRecipe is Recipe))
+      {
+        return false;
+      }
+      else
+      {
+        Recipe newRecipe = (Recipe) otherRecipe;
+        bool idEquality = (this.GetId() == newRecipe.GetId());
+        bool nameEquality = (this.GetName() == newRecipe.GetName());
+        bool ratingEquality = (this.GetRating() == newRecipe.GetRating());
+        bool instructionEquality = (this.GetInstruction() == newRecipe.GetInstruction());
+        return (idEquality && nameEquality && ratingEquality && instructionEquality);
+      }
+    }
+
+    public override int GetHashCode()
+    {
+      return this.GetId().GetHashCode();
+    }
+
+    public int GetId()
+    {
+      return _id;
+    }
+
+    public void SetId(int newId)
+    {
+      _id = newId;
+    }
+
+    public string GetName()
+    {
+      return _name;
+    }
+
+    public void SetName(string newRecipeName)
+    {
+      _name = newRecipeName;
+    }
+
+    public string GetRating()
+    {
+      return _rating;
+    }
+
+    public void SetRating(string newRating)
+    {
+      _rating = newRating;
+    }
+
+    public string GetInstruction()
+    {
+      return _instruction;
+    }
+
+    public void SetInstruction(string newInstruction)
+    {
+      _instruction = newInstruction;
+    }
+
+
+    public void Save()
+    {
+      MySqlConnection conn = DB.Connection();
+     conn.Open();
+
+     var cmd = conn.CreateCommand() as MySqlCommand;
+    cmd.CommandText = @"INSERT INTO recipes (name) VALUES (@RecipeName);";
+     cmd.Parameters.Add(new MySqlParameter("@RecipeName", this._name));
+
+     cmd.ExecuteNonQuery();
+     _id = (int) cmd.LastInsertedId;
+
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+    }
+
+    public static Recipe Find(int id)
+    {
+     MySqlConnection conn = DB.Connection();
+     conn.Open();
+
+     var cmd = conn.CreateCommand() as MySqlCommand;
+     cmd.CommandText = @"SELECT * FROM recipes WHERE id = @searchId;";
+
+     MySqlParameter searchId = new MySqlParameter();
+     searchId.ParameterName = "@searchId";
+     searchId.Value = id;
+     cmd.Parameters.Add(searchId);
+
+     var rdr = cmd.ExecuteReader() as MySqlDataReader;
+
+     int recipeId = 0;
+     string recipeName = "";
+     string recipeRating = "";
+     string recipeInstruction = "";
+
+     while (rdr.Read())
+     {
+      recipeId = rdr.GetInt32(0);
+      recipeName = rdr.GetString(1);
+      recipeRating = rdr.GetString(2);
+      recipeInstruction = rdr.GetString(3);
+     }
+
+     Recipe foundRecipe= new Recipe(recipeName, recipeRating, recipeInstruction, recipeId);
+
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+
+     return foundRecipe;
+    }
+
+    public void EditName(string newName)
+    {
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"UPDATE recipes SET name = @newName WHERE id = @searchId;";
+
+      cmd.Parameters.Add(new MySqlParameter("@searchId", _id));
+
+      cmd.Parameters.Add(new MySqlParameter("@newname", newName));
+
+      cmd.ExecuteNonQuery();
+      _name = newName;
+
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+    }
+
+    public void EditRating(string newRating)
+    {
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"UPDATE recipes SET rating = @newRating WHERE id = @searchId;";
+
+      cmd.Parameters.Add(new MySqlParameter("@searchId", _id));
+
+      cmd.Parameters.Add(new MySqlParameter("@newrating", newRating));
+
+      cmd.ExecuteNonQuery();
+      _rating = newRating;
+
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+    }
+
+    public void EditInstruction(string newInstruction)
+    {
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"UPDATE recipes SET instruction = @newInstruction WHERE id = @searchId;";
+
+      cmd.Parameters.Add(new MySqlParameter("@searchId", _id));
+
+      cmd.Parameters.Add(new MySqlParameter("@newinstruction", newInstruction));
+
+      cmd.ExecuteNonQuery();
+      _instruction = newInstruction;
+
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+    }
+
+    public static List<Recipe> GetAll()
+    {
+      List<Recipe> allRecipes = new List<Recipe> {};
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"SELECT * FROM recipes;";
+      var rdr = cmd.ExecuteReader() as MySqlDataReader;
+      while(rdr.Read())
+      {
+        int recipeId = rdr.GetInt32(0);
+        string recipeName = rdr.GetString(1);
+        string recipeRating = rdr.GetString(2);
+        string recipeInstruction = rdr.GetString(3);
+        Recipe newRecipe = new Recipe(recipeName, recipeRating, recipeInstruction, recipeId);
+        allRecipes.Add(newRecipe);
+      }
+      conn.Close();
+      if (conn != null)
+      {
+          conn.Dispose();
+      }
+      return allRecipes;
+    }
+
+    public void DeleteRecipe(int id)
+    {
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"DELETE FROM recipes WHERE id = @searchId;";
+
+      MySqlParameter searchId = new MySqlParameter();
+      searchId.ParameterName = "@searchId";
+      searchId.Value = _id;
+      cmd.Parameters.Add(searchId);
+
+      cmd.ExecuteNonQuery();
+
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+    }
   }
 }
